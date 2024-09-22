83bf8f2c4def9539627150e461bc47d01bbb452c
Yaroslav Shmarov <yshmarov@gmail.com>
2024-09-22 23:28:31 +0300

    Strada Form
    ios: https://github.com/hotwired/turbo-ios/blob/main/Demo/Strada/FormComponent.swift
    
    html/rails: https://github.com/hotwired/turbo-native-demo/blob/main/public/javascript/controllers/bridge/form_controller.js

--- tinder-ios/tinder-ios/Components/BridgeComponent+App.swift
+++ tinder-ios/tinder-ios/Components/BridgeComponent+App.swift
@@ -2,7 +2,8 @@ import Strada
 extension BridgeComponent {
     static var allTypes: [BridgeComponent.Type] {
         [
-            ButtonComponent.self
+            ButtonComponent.self,
+            FormComponent.self
         ]
     }
 }
